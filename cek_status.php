70c04f01b8d8f2f3f14351b7230484185249d770
rizkyramadhan1601 <rizkyramadhanmuhammad16.com>
2026-02-07 18:03:00 +0700

    update

--- cek_status.php
+++ cek_status.php
@@ -50,7 +50,8 @@ if(isset($_POST['cek'])){
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    
+    <link href="assets-img/logo.webp" rel="icon">
+    <link href="assets-img/logo.webp" rel="apple-touch-icon">   
     <style>
         * {
             margin: 0;
--- join_member.php
+++ join_member.php
@@ -6,7 +6,8 @@
     <title>Join Member - EZParking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    
+    <link href="assets-img/logo.webp" rel="icon">
+    <link href="assets-img/logo.webp" rel="apple-touch-icon">
     <style>
         * {
             margin: 0;
